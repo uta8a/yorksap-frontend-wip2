c6567317b65db3cf62e509c304ffb2e4d5df1af7
Masanori Tani <uta8a@uta8a.org>
2023-12-23 10:54:07 +0000

    make view from roomlist

--- src/Pages/Description.elm
+++ src/Pages/Description.elm
@@ -1,23 +1,79 @@
 module Pages.Description exposing (page)
 
-import Html exposing (a, div, text)
-import Html.Attributes exposing (href)
+import Effect exposing (Effect)
+import Html exposing (Html, a, button, div, li, text, ul)
+import Html.Attributes exposing (href, id)
+import Html.Events exposing (onClick)
+import Maybe exposing (withDefault)
 import Shared exposing (Shared)
 import Spa.Page
 import View exposing (View)
 
 
-page : Shared -> Spa.Page.Page () Shared.Msg (View ()) () ()
-page shared =
-    Spa.Page.static (view shared)
+page : Shared -> Spa.Page.Page () Shared.Msg (View Msg) Model Msg
+page _ =
+    Spa.Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
 
 
-view : Shared -> View ()
-view _ =
+type Msg
+    = SetValue RoomList
+
+
+type alias Model =
+    { roomlist : RoomList
+    }
+
+
+type alias RoomList =
+    List Room
+
+
+type alias Room =
+    { id : String
+    , name : String
+    }
+
+
+
+-- init, update, view, subscriptions
+
+
+init : () -> ( Model, Effect Shared.Msg Msg )
+init _ =
+    Model [] |> Effect.withNone
+
+
+update : Msg -> Model -> ( Model, Effect Shared.Msg Msg )
+update msg model =
+    case msg of
+        SetValue roomlist ->
+            ( { model | roomlist = roomlist }, Effect.none )
+
+
+view : Model -> View Msg
+view model =
     { title = "Description"
     , body =
         div []
             [ text "This is the description page"
-            , div [] [ a [ href "/" ] [ text "Go back to the home page" ] ]
+            , button [ onClick (SetValue [ { id = "1", name = "room1" }, { id = "2", name = "room2" } ]) ] [ text "Click me" ]
+            , div [] [ text (withDefault { id = "null", name = "null" } (List.head model.roomlist)).name ]
+            , renderList model.roomlist
             ]
     }
+
+
+
+-- helper functions
+
+
+renderList : List Room -> Html Msg
+renderList lst =
+    lst
+        |> List.map (\l -> li [] [ text (l.id ++ " / " ++ l.name) ])
+        |> ul []
